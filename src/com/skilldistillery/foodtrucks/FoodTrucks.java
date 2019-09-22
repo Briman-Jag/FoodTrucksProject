95f4dc4e27987544757fde632ed8bd1b10fa8658
Briman-Jag <brianstreetman25@gmail.com>
2019-09-22 01:40:44 -0600

    Added code within FoodTrucks to call upon FoodTruck array, set max of 5, add trucks, and get food trucks

--- src/com/skilldistillery/foodtrucks/FoodTrucks.java
+++ src/com/skilldistillery/foodtrucks/FoodTrucks.java
@@ -2,6 +2,27 @@ package com.skilldistillery.foodtrucks;
 
 public class FoodTrucks {
 	
+	
 	private FoodTruck[] foodTrucks;
+	private int numTrucks = 0;
+	
 	private static final int MAX_TRUCKS = 5;
+	
+	public FoodTrucks() {
+		foodTrucks = new FoodTruck[MAX_TRUCKS];
+	}
+	
+	public void addTruck(FoodTrucks f) {
+		foodTrucks[numTrucks] = f;
+		numTrucks++;
+	}
+	
+	public FoodTrucks[] getFoodTrucks() {
+		FoodTrucks[] trucksCopy;
+		trucksCopy = new FoodTrucks[numTrucks];
+		for(int i = 0; i < numTrucks; i++) {
+			trucksCopy[i] = foodTrucks[i];
+		}
+		return trucksCopy;
+	}
 }
